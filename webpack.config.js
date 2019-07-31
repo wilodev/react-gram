c36d82946c4364c0a2157c511c1ba1e740c7eecb
Wilson Pérez <wilson@urbanroosters.com>
2019-07-31 12:58:03 -0500

    ADD: new func -> Add PWA

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
+const path = require('path')
 module.exports = {
   output: {
     filename: 'app.bundle.js',
@@ -23,6 +25,37 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
+    }),
+    new WebpackPwaManifestPlugin({
+      name: 'reactgram - app de fotos de mascotas',
+      short_name: 'reactgram',
+      description: 'this is a new app of pets',
+      background_color: '#fff',
+      theme_color: '#b1a',
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512]
+        }
+      ]
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('https://(res.cloudinary.com|images.unsplash.com)'),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images'
+          }
+        },
+        {
+          urlPattern: new RegExp('https://react-server-wilodev.wilodev.now.sh'),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api'
+          }
+        }
+      ]
     })
   ]
 }
